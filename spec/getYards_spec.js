93e97984311901f18912daa5a47d5572e8a4035c
Rphamily <rpham9987@gmail.com>
2025-01-24 02:30:47 -0500

    changed text

--- spec/getYards_spec.js
+++ spec/getYards_spec.js
@@ -4,14 +4,14 @@ describe("test meterstoyards", function() {
     it("test 10 m", function() {
         let meters = 10;
         let yards = meterstoyards(meters);
-        console.log(meters + "Meters is" + yards + "yards")
+        console.log(meters + " Meters is " + yards + " yards")
         expect(yards.toFixed(2)).toBe("10.94");
     });
 
     it("test 123 m", function() {
         let meters = 123;
         let yards = meterstoyards(meters);
-        console.log(meters + "Meters is" + yards + "yards")
+        console.log(meters + " Meters is " + yards + " yards")
         expect(yards.toFixed(2)).toBe("134.51");
     });
 });
